2e4a1bdbdbf98350c047f57d9d3cb7dced4a655a
Markus Frank <markus.frank@informatik.uni-stuttgart.de>
2019-11-21 13:08:49 +0100

    Update Main.java

--- src/de/unistuttgart/iste/rss/oo/hamstersimulator/exercise7/homework/Main.java
+++ src/de/unistuttgart/iste/rss/oo/hamstersimulator/exercise7/homework/Main.java
@@ -5,12 +5,7 @@ public class Main {
 	public static void main(String args[]) {
 		
 		// Your code goes here
-		ExceptionalHamsterGame hamster = new ExceptionalHamsterGame();
-		hamster.doRun();
-		
-		
-		//HungryPaule paule = new HungryPaule();
-		//paule.doRun();
+
 		
 	}
 	
